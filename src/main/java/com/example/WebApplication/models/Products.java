3a3e89c3648f023386a8c2867f4fa14ba46cb7b4
EGZ-ZS8-1 <maiklinfo@mail.ru>
2023-07-17 12:29:18 +0200

    Добавляем класс Products.Добавляем библтотеку lombok.

---
+++ src/main/java/com/example/WebApplication/models/Products.java
@@ -0,0 +1,12 @@
+package com.example.WebApplication.models;
+
+import lombok.Data;
+
+@Data
+public class Products {
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
