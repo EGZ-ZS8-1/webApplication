16eab4c2f5468c9d708c9f8b5cb6f90e5718253a
EGZ-ZS8-1 <maiklinfo@mail.ru>
2023-07-18 07:57:16 +0200

    Добавляем класс ProductsService.Заносим данные о первых продуктах

--- src/main/java/com/example/WebApplication/models/Products.java
+++ src/main/java/com/example/WebApplication/models/Products.java
@@ -1,8 +1,10 @@
 package com.example.WebApplication.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Products {
     private String title;
     private String description;
---
+++ src/main/java/com/example/WebApplication/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.WebApplication.services;
+
+import com.example.WebApplication.models.Products;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Products> products = new ArrayList<>();
+    private long ID = 0;
+    {
+        products.add(new Products("Помидоры", "Абаканские", 200, "Калининград", "Андрей"));
+        products.add(new Products("Огурцы", "Абаканские", 100, "Калининград", "Андрей"));
+    }
+
+    public List<Products> getProducts() {return products;}
+    public void saveProduct(Products product){
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProducts (Long id) {
+        products.removeIf(product ->product.getId().equals(id));
+    }
+}
