73921946cd75cd8ba9a0a16cbf5d1d6e3b9c23c5
EGZ-ZS8-1 <maiklinfo@mail.ru>
2023-08-04 13:47:16 +0200

    Работа над ошибками в ProductService.Переименование методов

--- src/main/java/com/example/WebApplication/services/ProductService.java
+++ src/main/java/com/example/WebApplication/services/ProductService.java
@@ -1,39 +1,37 @@
 package com.example.WebApplication.services;
 
-import com.example.WebApplication.models.Products;
-import lombok.Data;
+import com.example.WebApplication.models.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-
 public class ProductService {
-    private List<Products> products = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
     private long ID = 0;
     {
-        products.add(new Products(++ID,"Помидоры", "Абаканские", 200, "Калининград", "Андрей"));
-        products.add(new Products(++ID,"Огурцы", "Абаканские", 100, "Калининград", "Андрей"));
+        products.add(new Product(++ID,"Помидоры", "Абаканские", 200, "Калининград", "Андрей"));
+        products.add(new Product(++ID,"Огурцы", "Абаканские", 100, "Калининград", "Андрей"));
     }
 
-    public List<Products> getProducts() {return products;}
-    public void saveProduct(Products product){
+    public List<Product> listProducts() {return products;}
+
+    public void saveProduct(Product product){
         product.setId(++ID);
         products.add(product);
     }
 
-    public void deleteProducts (Long id) {
+    public void deleteProduct (Long id) {
         products.removeIf(product -> product.getId().equals(id));
     }
 
-
-
-
-    public Products getProductsById(Long id) {
-        for (Products product : products) {
+    public Product getProductById(Long id) {
+        for (Product product : products) {
             if (product.getId().equals(id)) return product;
         }
         return null;
     }
+
+
 }
