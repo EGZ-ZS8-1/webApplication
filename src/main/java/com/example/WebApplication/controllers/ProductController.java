59da0b496d7973a74c892147444cc442d4d4632e
EGZ-ZS8-1 <maiklinfo@mail.ru>
2023-08-04 13:46:38 +0200

    Работа над ошибками в ProductController. Переименование методов

--- src/main/java/com/example/WebApplication/controllers/ProductController.java
+++ src/main/java/com/example/WebApplication/controllers/ProductController.java
@@ -1,6 +1,6 @@
 package com.example.WebApplication.controllers;
 
-import com.example.WebApplication.models.Products;
+import com.example.WebApplication.models.Product;
 import com.example.WebApplication.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -14,33 +14,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ProductController {
     private final ProductService productService;
 
-//    public ProductController(ProductService productService) {
-//        this.productService = productService;
-//    }
-
     @GetMapping("/")
     public String products(Model model) {
-        model.addAttribute("products", productService.getProducts());
+        model.addAttribute("products", productService.listProducts());
         return "products";
     }
     @GetMapping("/product/${id}")
     public String productInfo(@PathVariable Long id, Model model){
-        model.addAttribute("product", productService.getProductsById(id));
+        model.addAttribute("product", productService.getProductById(id));
         return "product-info";
 
     }
-
     @PostMapping("/product/create")
-    public String createProducts(Products product) {
+    public String createProducts(Product product) {
      productService.saveProduct(product);
      return "redirect:/";
     }
     @PostMapping("/product/delete/${id}")
     public String deleteProducts (@PathVariable Long id) {
-        productService.deleteProducts(id);
+        productService.deleteProduct(id);
         return "redirect:/";
-
     }
-
-
 }
