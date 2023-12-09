8b0bd95ef10db9119066b977a4de8f297b19e933
EGZ-ZS8-1 <maiklinfo@mail.ru>
2023-12-09 16:26:17 +0200

    Work for ProductService

--- src/main/java/com/example/WebApplication/services/ProductService.java
+++ src/main/java/com/example/WebApplication/services/ProductService.java
@@ -1,32 +1,36 @@
 package com.example.WebApplication.services;
 
 import com.example.WebApplication.models.Product;
+import com.example.WebApplication.repositiries.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
 
-
-    public List<Product> listProducts() {return products;}
-
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+    public List<Product> listProducts(String title) {
+        if (title != null) productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public void deleteProduct (Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+    public void saveProduct(Product product) {
+        log.info("Saving new {}", product);
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        productRepository.findById(id).orElse(null);
     }
-
-
 }
+
+
